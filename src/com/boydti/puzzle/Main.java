7d41bb64d6fc5dc2c49ea41039964ce119aa56ca
boy0001 <jessepaleg@gmail.com>
2015-04-22 19:13:10 +1000

    .

--- src/com/boydti/puzzle/Main.java
+++ src/com/boydti/puzzle/Main.java
@@ -3,6 +3,12 @@ package com.boydti.puzzle;
 import java.util.Arrays;
 
 public class Main {
+    
+    /**
+     * Value of 1 is 100% accurate for A* and CUS2
+     * Higher values are faster but less accurate
+     */
+    public static int PRECISION = 5;
 	
 	public static void main(String[] args) {
 	    // The initial and goal states
@@ -14,11 +20,11 @@ public class Main {
 	     */
 	    
 	    // 3x5 -> 14 moves
-//	    initial = new byte[] { 1, 2, 3, 5, 11, 6, 4, 13, 9, 8, 0, 12, 7, 10, 14 };
+	    initial = new byte[] { 1, 2, 3, 5, 11, 6, 4, 13, 9, 8, 0, 12, 7, 10, 14 };
 	    
 	    // 3x3 -> 20 moves
-	    initial = new byte[] { 6, 7, 4, 1, 5, 3, 8, 0, 2 };
-	    goal = new byte[] {3, 4, 2, 1, 8, 7, 6, 0, 5};
+//	    initial = new byte[] { 6, 7, 4, 1, 5, 3, 8, 0, 2 };
+//	    goal = new byte[] {3, 4, 2, 1, 8, 7, 6, 0, 5};
 	    
 	    // 3x3 -> 31 moves
 //	    initial = new byte[] { 6,4,7,8,5,0,3,2,1 };
@@ -27,11 +33,11 @@ public class Main {
 //	    initial = new byte[] {0,9,3,7,1,5,4,8,2,6};
 	    
 	    /*
-	     * Algorithm BFSolver, DFSolver, GBFSolver
+	     * Algorithm BFS, DFS, GBFS, AS, CUS1, CUS2
 	     */
-		String solver = "BFSolver";
+		String solver = "BFS";
 		int width = 3;
-		int height = 3;
+		int height = 5;
 		
 		/*
 		 * Parsing command line arguments (so you can specify the solver etc)
@@ -65,6 +71,9 @@ public class Main {
 					}
 				}
 			}
+			else if (split.length == 0) {
+			    solver = arg;
+			}
 		}
 		// Setting the goal to the sorted byte array (if it wasn't specified)
 		if (goal == null) {
@@ -77,23 +86,20 @@ public class Main {
         }
 		
 		// Creating a new instance of the specified ASolver class
-		ASolver imp;
+		AbstractSolver imp;
 		try {
-			Class<?> clazz = Class.forName("com.boydti.puzzle." + solver);
-			imp = (ASolver) clazz.getConstructor(int.class, int.class, byte[].class, byte[].class).newInstance(width, height, initial, goal);
+			Class<?> clazz = Class.forName("com.boydti.puzzle." + solver + "Solver");
+			imp = (AbstractSolver) clazz.getConstructor(int.class, int.class, byte[].class, byte[].class).newInstance(width, height, initial, goal);
 		} catch (Exception e) {
 			e.printStackTrace();
-			imp = new BFSolver(width, height, initial, goal);
+			imp = new BFSSolver(width, height, initial, goal);
 		}
 		
 		imp.printState(imp.getState(initial));
 		imp.printState(imp.getState(goal));
 		
-		// General stats
-		System.out.println("Manhattan Distance: " + imp.manhattanDistance(imp.getState(initial)));
-		
 		// Actual code
-		System.out.println("Solving...");
+		System.out.println("Solving with " + solver);
 		long start = System.currentTimeMillis();
 
 		// Solving the puzzle
@@ -101,6 +107,7 @@ public class Main {
 		    imp.solve();
 		}
 		catch (Exception e) {
+		    e.printStackTrace();
 		    System.out.println(System.currentTimeMillis() - start);
 		    System.out.println("No solutions found!");
 		    return;
